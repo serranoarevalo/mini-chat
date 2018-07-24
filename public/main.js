b623ed79f812c978363d742db2d67f5698c14d51
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-25 02:07:07 +0300

    Appending messages to the client

--- public/main.js
+++ public/main.js
@@ -1,19 +1,29 @@
 const socket = io("http://localhost:4000"),
   form = document.querySelector(".js-form"),
+  messageList = document.querySelector(".js-messageList"),
   input = document.querySelector(".js-input");
+
+const YOURS_CLASS = "yours";
+const MINE_CLASS = "mine";
+
+const addMessage = (data, cssClass) => {
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = data;
+  newMessage.classList.add(cssClass);
+  messageList.appendChild(newMessage);
+};
+
+socket.on("new message sent", data => {
+  const { message } = data;
+  addMessage(message, YOURS_CLASS);
+});
 
 const submitMessage = event => {
   event.preventDefault();
   const inputValue = input.value;
   socket.emit("new message", inputValue);
+  addMessage(inputValue, MINE_CLASS);
+  input.value = "";
 };
 
 form.addEventListener("submit", submitMessage);
-
-const addMessage = data => {
-  console.log(data);
-};
-
-socket.on("new message sent", data => {
-  addMessage(data);
-});
