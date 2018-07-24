067410ab87c92d49e6cf0843189553ede8279f53
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-25 00:50:40 +0300

    Static Files + Socket Connection on client and server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"),
   socketIO = require("socket.io"),
   http = require("http"),
+  path = require("path"),
   app = express(),
   server = http.createServer(app),
   io = socketIO(server);
@@ -8,4 +9,12 @@ const express = require("express"),
 const PORT = 4000;
 const handleListening = () => console.log(`Server running on port ${PORT}`);
 
+const onSocketConnection = socket => {
+  console.log(socket);
+};
+
 server.listen(PORT, handleListening);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", onSocketConnection);
